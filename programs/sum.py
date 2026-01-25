a61a3e1977d068bc4357b207103a468d71df4e4f
Stefan Ghetu <stefanghetu9@gmail.com>
2026-01-25 20:04:24 +0200

    temporary fix cu pseudoinstructiunea NOP
    fix pana la implementarea de hazard handling, cel mai probabil se va lucra cu mecanismul de forwarding

--- programs/sum.py
+++ programs/sum.py
@@ -6,5 +6,9 @@ def get_program():
     return [
         Instruction("addi", rd=1, rs1=0, imm=5), #addi x1, x0, 5
         Instruction("addi", rd=2, rs1=0, imm=3), #addi x2, x0, 3
+        Instruction('addi', rd=0, rs1=0, imm=0), # nop #cu un nop x3 --> 5
+        #rezulta ce anume? evident se termina un singur wb
+        Instruction("addi", rd=0, rs1=0, imm=0), # nop cu acest nop x3 --> 8
+        #adica se termina si al doilea wb inainte de add
         Instruction("add", rd=3, rs1=1, rs2=2), #add x3, x1, x2
     ]
